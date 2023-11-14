ac8842d63badb4e06520db374000992024252610
Rarri74 <Hammed74@gmail.com>
2023-11-14 12:08:01 -0500

    re-wrote initial logic

--- index.js
+++ index.js
@@ -1,177 +1,77 @@
-const cells = {
-  cell1: document.querySelector(".board1"),
-  cell2: document.querySelector(".board2"),
-  cell3: document.querySelector(".board3"),
-  cell4: document.querySelector(".board4"),
-  cell5: document.querySelector(".board5"),
-  cell6: document.querySelector(".board6"),
-  cell7: document.querySelector(".board7"),
-  cell8: document.querySelector(".board8"),
-  cell9: document.querySelector(".board9"),
-};
-
-const marks = (function () {
-  const makeStar = (cell) => {
-    let star = document.createElement("img");
-    star.classList.add("mark");
-    star.src = "assets/star.png";
-    cell.appendChild(star);
-  };
-
-  const makeHeart = (cell) => {
-    let heart = document.createElement("img");
-    heart.classList.add("mark");
-    heart.src = "assets/heart.png";
-    cell.appendChild(heart);
-  };
-  return { makeStar, makeHeart };
-})();
-
-cells.cell1.addEventListener("click", () => {
-  if ((piecesOnBoard = 0 || piecesOnBoard % 2 === 0)) {
-    players.playerX.setPiece(1);
-    marks.makeStar(cells.cell1);
-  } else {
-    players.playerO.setPiece(1);
-    marks.makeHeart(cells.cell1);
-  }
-});
-
-cells.cell2.addEventListener("click", () => {
-  if ((piecesOnBoard = 0 || piecesOnBoard % 2 === 0)) {
-    players.playerX.setPiece(2);
-    marks.makeStar(cells.cell2);
-  } else {
-    players.playerO.setPiece(2);
-    marks.makeHeart(cells.cell2);
-  }
-});
-
-cells.cell3.addEventListener("click", () => {
-  if ((piecesOnBoard = 0 || piecesOnBoard % 2 === 0)) {
-    players.playerX.setPiece(3);
-    marks.makeStar(cells.cell3);
-  } else {
-    players.playerO.setPiece(3);
-    marks.makeHeart(cells.cell3);
-  }
-});
-
-cells.cell4.addEventListener("click", () => {
-  if ((piecesOnBoard = 0 || piecesOnBoard % 2 === 0)) {
-    players.playerX.setPiece(4);
-    marks.makeStar(cells.cell4);
-  } else {
-    players.playerO.setPiece(4);
-    marks.makeHeart(cells.cell4);
-  }
-});
-
-cells.cell5.addEventListener("click", () => {
-  if ((piecesOnBoard = 0 || piecesOnBoard % 2 === 0)) {
-    players.playerX.setPiece(5);
-    marks.makeStar(cells.cell5);
-  } else {
-    players.playerO.setPiece(5);
-    marks.makeHeart(cells.cell5);
-  }
-});
-
-cells.cell6.addEventListener("click", () => {
-  if ((piecesOnBoard = 0 || piecesOnBoard % 2 === 0)) {
-    players.playerX.setPiece(6);
-    marks.makeStar(cells.cell6);
-  } else {
-    players.playerO.setPiece(6);
-    marks.makeHeart(cells.cell6);
-  }
-});
-
-cells.cell7.addEventListener("click", () => {
-  if ((piecesOnBoard = 0 || piecesOnBoard % 2 === 0)) {
-    players.playerX.setPiece(7);
-    marks.makeStar(cells.cell7);
-  } else {
-    players.playerO.setPiece(7);
-    marks.makeHeart(cells.cell7);
-  }
-});
-
-cells.cell8.addEventListener("click", () => {
-  if ((piecesOnBoard = 0 || piecesOnBoard % 2 === 0)) {
-    players.playerX.setPiece(8);
-    marks.makeStar(cells.cell8);
-  } else {
-    players.playerO.setPiece(8);
-    marks.makeHeart(cells.cell8);
-  }
-});
-
-cells.cell9.addEventListener("click", () => {
-  if ((piecesOnBoard = 0 || piecesOnBoard % 2 === 0)) {
-    players.playerX.setPiece(9);
-    marks.makeStar(cells.cell9);
-  } else {
-    players.playerO.setPiece(9);
-    marks.makeHeart(cells.cell9);
-  }
-});
-
-const gameBoard = {
-  Gameboard: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-};
-
 function createPlayer(type) {
+  //create our player objects
   const playerType = type;
-  const setPiece = (x) => {
-    if (gameBoard.Gameboard[x - 1] === "o") {
-      console.log("You cant do that");
-      alert("You can't do that");
-    }
-    gameBoard.Gameboard[x - 1] = `${playerType}`;
-    console.log(gameBoard.Gameboard);
-    console.log(`Player Type is ${playerType}`);
-    if (
-      (gameBoard.Gameboard[0] === `${playerType}` &&
-        gameBoard.Gameboard[1] === `${playerType}` &&
-        gameBoard.Gameboard[2] === `${playerType}`) ||
-      (gameBoard.Gameboard[3] === `${playerType}` &&
-        gameBoard.Gameboard[4] === `${playerType}` &&
-        gameBoard.Gameboard[5] === `${playerType}`) ||
-      (gameBoard.Gameboard[6] === `${playerType}` &&
-        gameBoard.Gameboard[7] === `${playerType}` &&
-        gameBoard.Gameboard[8] === `${playerType}`) ||
-      (gameBoard.Gameboard[0] === `${playerType}` &&
-        gameBoard.Gameboard[3] === `${playerType}` &&
-        gameBoard.Gameboard[6] === `${playerType}`) ||
-      (gameBoard.Gameboard[1] === `${playerType}` &&
-        gameBoard.Gameboard[4] === `${playerType}` &&
-        gameBoard.Gameboard[7] === `${playerType}`) ||
-      (gameBoard.Gameboard[2] === `${playerType}` &&
-        gameBoard.Gameboard[5] === `${playerType}` &&
-        gameBoard.Gameboard[8] === `${playerType}`) ||
-      (gameBoard.Gameboard[0] === `${playerType}` &&
-        gameBoard.Gameboard[4] === `${playerType}` &&
-        gameBoard.Gameboard[8] === `${playerType}`) ||
-      (gameBoard.Gameboard[2] === `${playerType}` &&
-        gameBoard.Gameboard[4] === `${playerType}` &&
-        gameBoard.Gameboard[6] === `${playerType}`)
-    ) {
-      alert(`Player ${playerType} wins!`);
-      console.log(`Player ${playerType} wins!`);
-    }
-  };
-  return { type, setPiece };
+  return { playerType };
 }
+const cells = document.querySelectorAll(".cell");
+const cellsArray = Array.from(cells); //create an Array from our cells
 
 const players = {
   playerX: createPlayer("x"),
   playerO: createPlayer("o"),
 };
 
-function countPieces(array, ...values) {
-  const numOfPieces = array.filter((item) => values.includes(item));
-  return numOfPieces.length;
-}
+const createGameBoard = (function () {
+  const board = Array(9).fill(null);
+  const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
-let piecesOnBoard = countPieces(gameBoard.Gameboard, "x", "o");
+  const checkWinner = function (piece) {
+    winningCombos.forEach((combo) => {
+      const [a, b, c] = combo;
+      if (
+        board[a] &&
+        board[a] === board[b] &&
+        board[a] === board[c] &&
+        board[a] === piece
+      ) {
+        console.log(piece + " has won!");
+        board.fill(0,null,9)
+        isOver = true
+      } else {
+        return;
+      }
+    });
+    
+  };
+  let isOver = false;
+  return { board, checkWinner, isOver };
+})();
+
+const gameFlow = function () {
+  let currentPlayer = players.playerX;
+  const startGame = () => {
+    cells.forEach((cell) => {
+      cell.addEventListener("click", (event) => {
+        const id = event.target.id;
+        if (currentPlayer === players.playerX && createGameBoard.isOver === false) {
+          createGameBoard.board[id - 1] = "x";
+          cell.style.backgroundColor = "black";
+          currentPlayer = players.playerO;
+          createGameBoard.checkWinner("x");
+          console.log(createGameBoard.board)
+        } else if(currentPlayer = players.playerO && createGameBoard.isOver === false){
+          createGameBoard.board[id - 1] = "o";
+          cell.style.backgroundColor = "green";
+          currentPlayer = players.playerX;
+          createGameBoard.checkWinner("o");
+          console.log(createGameBoard.board);
+        } else if(createGameBoard.isOver === true){
+            for(i=0;i<10;i++){
+                cellsArray[i].style.backgroundColor = "white"
+            }
+        }
+      });
+    });
+  };
+  return startGame();
+};
+gameFlow();
